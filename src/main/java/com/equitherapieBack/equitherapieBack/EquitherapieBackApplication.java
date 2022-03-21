cd13fc87cd6180fe96a6d8e977f6d49c9bd7609b
godeline <godelineboy@gmail.com>
2022-03-21 11:12:11 +0100

    first commit

---
+++ src/main/java/com/equitherapieBack/equitherapieBack/EquitherapieBackApplication.java
@@ -0,0 +1,13 @@
+package com.equitherapieBack.equitherapieBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquitherapieBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquitherapieBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equitherapieBack/equitherapieBack/EquitherapieBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equitherapieBack.equitherapieBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquitherapieBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
